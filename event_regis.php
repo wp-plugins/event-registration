e707b4881ffc1ac61d0a7a7a9a137705ed5e2131
avdude <avdude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-04 21:16:57 +0000

    tagging version 3.042
    git-svn-id: https://plugins.svn.wordpress.org/event-registration/tags/3.042@115544 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- event_regis.php
+++ event_regis.php
@@ -13,8 +13,8 @@ persons contact information to a database and provides an association to an
 events database. It provides the ability to send the register to your 
 paypal payment site for online collection of event fees. Reporting features 
 provide a list of events, list of attendees, and excel export.
-Version: 3.03
-Author: David Fleming - Edge Technology Consulting, Inna Janssen - Janssen-Webservice
+Version: 3.041
+Author: David Fleming - Edge Technology Consulting
 Author URI: http://www.avdude.com
 */
 /*  Copyright 2008  DAVID_FLEMING  (email : CONSULTANT@AVDUDE.COM)
@@ -45,11 +45,11 @@ Author URI: http://www.avdude.com
 //Define the table versions for unique tables required in Events Registration
 
 
-$events_attendee_tbl_version = "3.04";
-$events_detail_tbl_version = "3.04";
-$events_organization_tbl_version = "3.04";
-$events_answer_tbl_version = "3.04";
-$events_question_tbl_version = "3.04";
+$events_attendee_tbl_version = "3.042";
+$events_detail_tbl_version = "3.042";
+$events_organization_tbl_version = "3.042";
+$events_answer_tbl_version = "3.042";
+$events_question_tbl_version = "3.042";
 
 /** this does not only affect language but also format of date, and which fields are displayes in the form */
 $lang_flag = "en"; //switch to en for changing language and form 
