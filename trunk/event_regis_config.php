822a6b02ae59782c595536c59fcf49b95a884e3f
avdude <avdude@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-27 06:02:58 +0000

    fixing issues
    git-svn-id: https://plugins.svn.wordpress.org/event-registration/tags/5.22@281422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/event_regis_config.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Edge Technology Consulting
+ * @copyright 2009
+ */
+
+//Define the table versions for unique tables required in Events Registration
+
+$events_attendee_tbl_version = "5.1";
+$events_detail_tbl_version = "5.1";
+$events_organization_tbl_version = "5.1";
+$events_answer_tbl_version = "5.1";
+$events_question_tbl_version = "5.1";
+
+/** this does not only affect language but also format of date, and which fields are displayes in the form */
+$events_lang_flag = "en"; //switch to en for changing language and form 
+
+
+/** defines the line break */
+define ( 'BR', '<br />' );
+
+?>
